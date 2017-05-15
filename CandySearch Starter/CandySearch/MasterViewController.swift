1d8e7507ca8e17910c78d56e5ab20940f5a2fef7
张建宇 <291206326@qq.com>
2017-05-15 17:35:41 +0800

    https://www.raywenderlich.com/113772/uisearchcontroller-tutorial

---
+++ CandySearch Starter/CandySearch/MasterViewController.swift
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2015 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var detailViewController: DetailViewController? = nil
+    var candies = [
+        Candy(category:"Chocolate", name:"Chocolate Bar"),
+        Candy(category:"Chocolate", name:"Chocolate Chip"),
+        Candy(category:"Chocolate", name:"Dark Chocolate"),
+        Candy(category:"Hard", name:"Lollipop"),
+        Candy(category:"Hard", name:"Candy Cane"),
+        Candy(category:"Hard", name:"Jaw Breaker"),
+        Candy(category:"Other", name:"Caramel"),
+        Candy(category:"Other", name:"Sour Chew"),
+        Candy(category:"Other", name:"Gummi Bear")
+    ]
+    let searchController = UISearchController.init(searchResultsController: nil)
+    var filteredCandies = [Candy]()
+    // MARK: - View Setup
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let splitViewController = splitViewController {
+            let controllers = splitViewController.viewControllers
+            detailViewController = (controllers[controllers.count - 1] as! UINavigationController).topViewController as? DetailViewController
+        }
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
+        searchController.searchBar.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredCandies = candies.filter { candy in
+            let categoryMatch = (scope == "All") || (candy.category == scope)
+            return  categoryMatch && candy.name.lowercased().contains(searchText.lowercased())
+        }
+        
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table View
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredCandies.count
+        }
+        return candies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let candy: Candy
+        if searchController.isActive && searchController.searchBar.text != "" {
+            candy = filteredCandies[indexPath.row]
+        } else {
+            candy = candies[indexPath.row]
+        }
+        cell.textLabel?.text = candy.name
+        cell.detailTextLabel?.text = candy.category
+        return cell
+        
+    }
+    
+    // MARK: - Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let candy: Candy
+                if searchController.isActive && searchController.searchBar.text != "" {
+                    candy = filteredCandies[indexPath.row]
+                } else {
+                    candy = candies[indexPath.row]
+                }
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailCandy = candy
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+}
+
+extension MasterViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+}
+
+extension MasterViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchText: searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# StudyOnRaywenderlich
